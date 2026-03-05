e8d75be520125d06dd9bcbed0efa41e511b9d155
plozdev <hoangmai.it.dev@gmail.com>
2026-03-05 11:14:46 +0700

    feat: add authentication request and response DTOs

---
+++ src/main/java/plozdev/todolistapi/dto/auth/LoginRequest.java
@@ -0,0 +1,13 @@
+package plozdev.todolistapi.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
