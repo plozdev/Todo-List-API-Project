40f3def93c23dd6a54c77cdc936a1fb2d5aa1a8f
plozdev <hoangmai.it.dev@gmail.com>
2026-03-05 13:58:50 +0700

    refactor: add app startup log

--- src/main/java/plozdev/todolistapi/TodoListApiApplication.java
+++ src/main/java/plozdev/todolistapi/TodoListApiApplication.java
@@ -1,13 +1,24 @@
 package plozdev.todolistapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@Slf4j
 public class TodoListApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoListApiApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        log.info("=================================================");
+        log.info("Swagger UI: http://localhost:8080/swagger-ui/index.html");
+        log.info("JSON: http://localhost:8080/v3/api-docs");
+        log.info("=================================================");
+    }
 }
