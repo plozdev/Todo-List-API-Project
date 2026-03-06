e378fe019efdcd50a3a77c6b455c833766ec1f18
plozdev <hoangmai.it.dev@gmail.com>
2026-03-06 22:41:52 +0700

    feat: enhance TaskRepository with pagination and custom query methods

--- src/main/java/plozdev/todolistapi/repository/TaskRepository.java
+++ src/main/java/plozdev/todolistapi/repository/TaskRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import plozdev.todolistapi.entities.Task;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository <Task, Integer>, JpaSpecificationExecutor<Task>  {
     Page<Task> findByUserId(Integer userId, Pageable pageable);
     Page<Task> findByUserIdAndIsCompleted(Integer userId, boolean isCompleted, Pageable pageable);
-
+    Optional<Task> findTaskById(Integer id);
 }
