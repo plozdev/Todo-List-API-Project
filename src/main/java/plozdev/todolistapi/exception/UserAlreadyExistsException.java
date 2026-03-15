1bec006f1235b98b0ce53dc58fc498a98eedd1c2
plozdev <hoangmai.it.dev@gmail.com>
2026-03-15 18:33:42 +0700

    feat(exception): add custom exception classes for auth

---
+++ src/main/java/plozdev/todolistapi/exception/UserAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package plozdev.todolistapi.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause ) {
+        super(message, cause);
+    }
+}
